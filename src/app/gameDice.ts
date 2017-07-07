5b12ca2a0caf175f2f4c8ad01f8bcd563d59edf1
Hobie <doschev@gmail.com>
2017-07-06 20:33:05 -0400

    Game Dice Test is working.

---
+++ src/app/gameDice.ts
@@ -0,0 +1,15 @@
+export class GameDice {
+    
+    private minValue: number = 1;
+    private maxValue: number = 6;
+
+    public roll(): number {
+        const diOne = (Math.floor(Math.random()
+            * (this.maxValue - this.minValue + 1)) + this.minValue);
+
+        const diTwo = (Math.floor(Math.random()
+            * (this.maxValue - this.minValue + 1)) + this.minValue);
+
+        return diOne + diTwo;
+    }
+}
---
+++ src/app/testDice.ts
@@ -0,0 +1,13 @@
+export class TestDice implements Dice {
+    public rollOne: number;
+    public rollTwo: number;
+
+    public roll(): number {
+        return this.rollOne + this.rollTwo;
+    }
+
+    public load(rollOne: number, rollTwo: number): void {
+        this.rollOne = rollOne;
+        this.rollTwo = rollTwo;
+    }
+}
